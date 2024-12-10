1d914142209d99b38a558c6041e84ca7de0f5a8a
Varshith V Hegde <varshithvh@gmail.com>
2024-12-10 17:35:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="mf4processor",
-    version="1.0.0",
+    version="2.0.1",
     description="A Python library for processing MF4 files",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Your Name",
-    author_email="your_email@example.com",
-    url="https://github.com/yourusername/mf4processor",  # Update with your repo link
+    author="Varshith V Hegde",
+    author_email="varshithvh@gmail.com",
+    url="https://github.com/varshithvhegde/mf4processor",  # Update with your repo link
     packages=find_packages(),
     install_requires=[
         "asammdf",
